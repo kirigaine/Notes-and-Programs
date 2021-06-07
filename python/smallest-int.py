e3b8a60776e12c2461c5a6f388bcef76f87e6901
Kirigaine <tholen2147@gmail.com>
2021-06-06 20:38:30 -0500

    Added comments, added test case with smallest missing int being max + 1

--- python/smallest-int.py
+++ python/smallest-int.py
@@ -12,22 +12,29 @@
 
 def main():
 
+    # Hard code the data entry
     array_a = [1,2,4,5,7,2,6]
+    array_b = [1,2,3,4,5,6,7]
     print(f"Smallest Integer Missing: {smallest_in_array(array_a)}")
+    print(f"Smallest Integer Missing: {smallest_in_array(array_b)}")
+
 
 def smallest_in_array(my_array):
     dict_a = {}
     smallest_int = 1
 
+    # If nothing in array, smallest int missing is 1
     if len(my_array) == 0:
         return 1
 
+    # If value in array is in dictionary, increment. Otherwise add to dictionary
     for value in my_array:
         if value in dict_a:
             dict_a.update({value:(dict_a[value])+1})
         else:
             dict_a.update({value: 1})
 
+    # Count to max value of array, smallest int missing is one that doesnt occur in array
     for i in range(1,max(my_array)+2):
         if i not in dict_a:
             smallest_int = i
